7dc390586a2919017aaf29ebde9725e4c13af0de
Andrew McWilliams <andrew.mcwilliams95@gmail.com>
2020-07-24 20:01:36 +0100

    Required attributes on model.

--- src/Models/User.cs
+++ src/Models/User.cs
@@ -1,11 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FiftyFiveClubApi.Models
 {
     public class User 
     {
         public long Id { get; set; }
+
+        [Required]
         public string FirstName { get; set; }
         public string SecondName { get; set; }
+        [Required]
         public string EmailAddress { get; set; }
-        public string Caption { get; set; }
+        public string Caption { get; set; } = "Doesnt Fancy a Caption";
     }
 }
